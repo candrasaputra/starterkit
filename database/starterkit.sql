37923cc923ca00fa2707e1bee0a8be6b880d70d9
candra saputra <candrasaputra@users.noreply.github.com>
2018-07-22 08:36:35 +0700

    new DB

--- database/starterkit.sql
+++ database/starterkit.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.4
+-- version 4.8.0.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1:3306
--- Generation Time: Feb 27, 2018 at 09:07 AM
--- Server version: 5.7.19
--- PHP Version: 5.6.31
+-- Host: 127.0.0.1
+-- Generation Time: Jul 22, 2018 at 03:35 AM
+-- Server version: 10.1.31-MariaDB
+-- PHP Version: 5.6.36
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -28,13 +28,11 @@ SET time_zone = "+00:00";
 -- Table structure for table `groups`
 --
 
-DROP TABLE IF EXISTS `groups`;
-CREATE TABLE IF NOT EXISTS `groups` (
-  `id` mediumint(8) UNSIGNED NOT NULL AUTO_INCREMENT,
+CREATE TABLE `groups` (
+  `id` mediumint(8) UNSIGNED NOT NULL,
   `name` varchar(20) NOT NULL,
-  `description` varchar(100) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+  `description` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `groups`
@@ -52,13 +50,11 @@ INSERT INTO `groups` (`id`, `name`, `description`) VALUES
 -- Table structure for table `login_attempts`
 --
 
-DROP TABLE IF EXISTS `login_attempts`;
-CREATE TABLE IF NOT EXISTS `login_attempts` (
-  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+CREATE TABLE `login_attempts` (
+  `id` int(11) UNSIGNED NOT NULL,
   `ip_address` varchar(15) NOT NULL,
   `login` varchar(100) NOT NULL,
-  `time` int(11) UNSIGNED DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  `time` int(11) UNSIGNED DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -67,9 +63,8 @@ CREATE TABLE IF NOT EXISTS `login_attempts` (
 -- Table structure for table `logsecurity`
 --
 
-DROP TABLE IF EXISTS `logsecurity`;
-CREATE TABLE IF NOT EXISTS `logsecurity` (
-  `lsID` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+CREATE TABLE `logsecurity` (
+  `lsID` int(11) UNSIGNED NOT NULL,
   `lsAction` varchar(30) NOT NULL,
   `lsIp` varchar(45) DEFAULT NULL,
   `lsPlatform` varchar(45) DEFAULT NULL,
@@ -77,9 +72,8 @@ CREATE TABLE IF NOT EXISTS `logsecurity` (
   `lsBrowserVersion` varchar(45) DEFAULT NULL,
   `lsDescription` varchar(100) DEFAULT NULL,
   `createdDate` datetime NOT NULL,
-  `createdUserID` int(11) UNSIGNED DEFAULT NULL,
-  PRIMARY KEY (`lsID`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+  `createdUserID` int(11) UNSIGNED DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `logsecurity`
@@ -93,7 +87,9 @@ INSERT INTO `logsecurity` (`lsID`, `lsAction`, `lsIp`, `lsPlatform`, `lsBrowser`
 (5, 'login_berhasil', '::1', 'Windows 10', 'Firefox', '58.0', '', '2018-02-27 15:58:13', 1),
 (6, 'logout_berhasil', '::1', 'Windows 10', 'Firefox', '58.0', '', '2018-02-27 15:59:57', 1),
 (7, 'login_gagal', '::1', 'Windows 10', 'Firefox', '58.0', '', '2018-02-27 16:04:21', 1),
-(8, 'login_berhasil', '::1', 'Windows 10', 'Firefox', '58.0', '', '2018-02-27 16:04:26', 1);
+(8, 'login_berhasil', '::1', 'Windows 10', 'Firefox', '58.0', '', '2018-02-27 16:04:26', 1),
+(9, 'logout_berhasil', '::1', 'Windows 10', 'Firefox', '61.0', '', '2018-07-22 08:33:48', 1),
+(10, 'login_berhasil', '::1', 'Windows 10', 'Firefox', '61.0', '', '2018-07-22 08:33:52', 1);
 
 -- --------------------------------------------------------
 
@@ -101,13 +97,11 @@ INSERT INTO `logsecurity` (`lsID`, `lsAction`, `lsIp`, `lsPlatform`, `lsBrowser`
 -- Table structure for table `roles`
 --
 
-DROP TABLE IF EXISTS `roles`;
-CREATE TABLE IF NOT EXISTS `roles` (
-  `roleID` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+CREATE TABLE `roles` (
+  `roleID` int(11) UNSIGNED NOT NULL,
   `roleName` varchar(40) NOT NULL,
-  `roleAlias` varchar(50) NOT NULL,
-  PRIMARY KEY (`roleID`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+  `roleAlias` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `roles`
@@ -122,15 +116,11 @@ INSERT INTO `roles` (`roleID`, `roleName`, `roleAlias`) VALUES
 -- Table structure for table `roles_groups`
 --
 
-DROP TABLE IF EXISTS `roles_groups`;
-CREATE TABLE IF NOT EXISTS `roles_groups` (
-  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+CREATE TABLE `roles_groups` (
+  `id` int(11) UNSIGNED NOT NULL,
   `roleID` int(11) UNSIGNED NOT NULL,
-  `group_id` mediumint(8) UNSIGNED NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `FK_roles_groups_groups` (`group_id`),
-  KEY `FK_roles_groups_roles` (`roleID`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+  `group_id` mediumint(8) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `roles_groups`
@@ -139,7 +129,8 @@ CREATE TABLE IF NOT EXISTS `roles_groups` (
 INSERT INTO `roles_groups` (`id`, `roleID`, `group_id`) VALUES
 (1, 1, 1),
 (2, 1, 2),
-(3, 1, 3);
+(3, 1, 3),
+(4, 1, 4);
 
 -- --------------------------------------------------------
 
@@ -147,9 +138,8 @@ INSERT INTO `roles_groups` (`id`, `roleID`, `group_id`) VALUES
 -- Table structure for table `users`
 --
 
-DROP TABLE IF EXISTS `users`;
-CREATE TABLE IF NOT EXISTS `users` (
-  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+CREATE TABLE `users` (
+  `id` int(11) UNSIGNED NOT NULL,
   `ip_address` varchar(45) NOT NULL,
   `username` varchar(100) NOT NULL,
   `password` varchar(255) NOT NULL,
@@ -169,17 +159,15 @@ CREATE TABLE IF NOT EXISTS `users` (
   `outletID` int(11) NOT NULL,
   `roleID` int(11) NOT NULL,
   `image` varchar(20) DEFAULT 'default.jpg',
-  `changePwd` tinyint(1) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `username` (`username`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+  `changePwd` tinyint(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `users`
 --
 
 INSERT INTO `users` (`id`, `ip_address`, `username`, `password`, `salt`, `email`, `activation_code`, `forgotten_password_code`, `forgotten_password_time`, `remember_code`, `created_on`, `last_login`, `active`, `first_name`, `last_name`, `company`, `phone`, `outletID`, `roleID`, `image`, `changePwd`) VALUES
-(1, '::1', 'administrator', '$2y$08$FYgj5tOTQRoSGij.OY0enepvAg2RUscXFP/FH9NPscsuuw9oSodgu', NULL, 'candrasaputra@live.com', NULL, NULL, NULL, NULL, 1268889823, 1519722266, 2, 'Candra Saputra', '-', 'Staf', '81373961140', 34, 1, 'default.jpg', 1);
+(1, '::1', 'administrator', '$2y$08$FYgj5tOTQRoSGij.OY0enepvAg2RUscXFP/FH9NPscsuuw9oSodgu', NULL, 'candrasaputra@live.com', NULL, NULL, NULL, NULL, 1268889823, 1532223232, 2, 'Candra Saputra', '-', 'Staf', '81373961140', 34, 1, 'default.jpg', 1);
 
 -- --------------------------------------------------------
 
@@ -187,16 +175,116 @@ INSERT INTO `users` (`id`, `ip_address`, `username`, `password`, `salt`, `email`
 -- Table structure for table `users_groups`
 --
 
-DROP TABLE IF EXISTS `users_groups`;
-CREATE TABLE IF NOT EXISTS `users_groups` (
-  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+CREATE TABLE `users_groups` (
+  `id` int(11) UNSIGNED NOT NULL,
   `user_id` int(11) UNSIGNED NOT NULL,
-  `group_id` mediumint(8) UNSIGNED NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `uc_users_groups` (`user_id`,`group_id`),
-  KEY `fk_users_groups_users1_idx` (`user_id`),
-  KEY `fk_users_groups_groups1_idx` (`group_id`)
+  `group_id` mediumint(8) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `users_groups`
+--
+
+INSERT INTO `users_groups` (`id`, `user_id`, `group_id`) VALUES
+(1, 1, 1);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `groups`
+--
+ALTER TABLE `groups`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `login_attempts`
+--
+ALTER TABLE `login_attempts`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `logsecurity`
+--
+ALTER TABLE `logsecurity`
+  ADD PRIMARY KEY (`lsID`);
+
+--
+-- Indexes for table `roles`
+--
+ALTER TABLE `roles`
+  ADD PRIMARY KEY (`roleID`);
+
+--
+-- Indexes for table `roles_groups`
+--
+ALTER TABLE `roles_groups`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `FK_roles_groups_groups` (`group_id`),
+  ADD KEY `FK_roles_groups_roles` (`roleID`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `username` (`username`);
+
+--
+-- Indexes for table `users_groups`
+--
+ALTER TABLE `users_groups`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `uc_users_groups` (`user_id`,`group_id`),
+  ADD KEY `fk_users_groups_users1_idx` (`user_id`),
+  ADD KEY `fk_users_groups_groups1_idx` (`group_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `groups`
+--
+ALTER TABLE `groups`
+  MODIFY `id` mediumint(8) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `login_attempts`
+--
+ALTER TABLE `login_attempts`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `logsecurity`
+--
+ALTER TABLE `logsecurity`
+  MODIFY `lsID` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `roles`
+--
+ALTER TABLE `roles`
+  MODIFY `roleID` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `roles_groups`
+--
+ALTER TABLE `roles_groups`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `users_groups`
+--
+ALTER TABLE `users_groups`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- Constraints for dumped tables
