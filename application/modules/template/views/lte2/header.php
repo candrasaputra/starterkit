72ebdb80ea28ef398226c92773b661095326ad28
candra saputra <candrasaputra@users.noreply.github.com>
2018-09-03 02:44:59 +0700

    up

--- application/modules/template/views/lte2/header.php
+++ application/modules/template/views/lte2/header.php
@@ -1,10 +1,10 @@
 <header class="main-header">
   <!-- Logo -->
-  <a href="https://pedasabis.com" class="logo">
+  <a href="<?php echo base_url(); ?>" class="logo">
     <!-- mini logo for sidebar mini 50x50 pixels -->
-    <span class="logo-mini"><b>SS</b></span>
+    <span class="logo-mini"><b>MENU</b></span>
     <!-- logo for regular state and mobile devices -->
-    <span class="logo-lg">PEDASABIS <strong>SS</strong></span>
+    <span class="logo-lg">MAIN <strong>MENU</strong></span>
   </a>
   <!-- Header Navbar: style can be found in header.less -->
   <nav class="navbar navbar-static-top" role="navigation">
