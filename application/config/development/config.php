d8d7e034a3c1b4054716874694f89f9db199e13c
candra saputra <candrasaputra@users.noreply.github.com>
2018-10-08 22:45:09 +0700

    add composer

--- application/config/development/config.php
+++ application/config/development/config.php
@@ -1,6 +1,8 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
+include_once 'vendor/autoload.php';
+
 /*
 |--------------------------------------------------------------------------
 | Base Site URL
